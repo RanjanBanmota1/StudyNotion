f4bbac1be2a8b6b9e4f14b34c08cc421420ce0cf
RanjanBanmota1 <Ranjanbanmotanitj@gmail.com>
2025-08-05 03:27:19 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:5173",
+		origin:[
+		"http://localhost:5173",
+		"http://localhost:5174", 
+	],
+		
 		credentials:true,
 	})
 )
