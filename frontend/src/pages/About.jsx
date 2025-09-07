6b8b9ce22577e58d70a502d4c18323db9b0a0ab1
RanjanBanmota1 <Ranjanbanmotanitj@gmail.com>
2025-09-08 02:28:07 +0530

    error resolved

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import HighlightText from '../components/core/HomePage/HighlightText';
+import HighLightText from '../components/core/HomePage/HighLightText';
 import BannerImage1 from "../assets/Images/aboutus1.webp"
 import BannerImage2 from "../assets/Images/aboutus2.webp"
 import BannerImage3 from "../assets/Images/aboutus3.webp"
@@ -20,7 +20,7 @@ const About = () => {
         <div className='relative mx-auto flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-center text-white'>
             <header className='mx-auto py-20 text-4xl font-semibold lg:w-[70%]'>
                 Driving Innovation in Online Education for a 
-                <HighlightText text={"Brighter Future"}/>
+                <HighLightText text={"Brighter Future"}/>
                 <p className='mx-auto mt-3 text-center text-base font-medium text-richblack-300 lg:w-[95%]'>Studynotion is at the forefront of driving innovation in online education. We're passionate about creating a brighter future by offering cutting-edge courses, leveraging emerging technologies, and nurturing a vibrant learning community.</p>
             </header>
             <div className='sm:h-[70px] lg:h-[150px]'></div>
